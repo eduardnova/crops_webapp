ceae1e8457264adc8815f3704f870c250ca78e95
Eduardo <rubeneduardonova@gmail.com>
2023-02-14 20:57:11 +0000

    Add somethings auth functions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import firebase_admin
 from flask import Flask, request, redirect, render_template
 from google.cloud import firestore, storage
-from firebase_admin import auth
+from firebase_admin import credentials, auth
 
 
 
@@ -43,12 +43,23 @@ def login_clientes():
        try:
           user = auth.sign_in_with_email_and_password(email = email, password = password)
        except Exception as e:
-          return "Inicio de session fallido"
+           return "Inicio de session fallido"
        # almacena el ID del usuarioi en la session
        session["user_id"] = user.uid
        return redirect("/") 
-    return render_template("loginclientes.html")   
-     
+    return render_template("loginclientes.html")  
+
+@app.route("/reset-passwordcliente",methods=["GET","POST"])
+def reset_password_cliente():     
+    if request.method == "POST": 
+       # obtiene el correo  electronico del formulario
+       email = request.form["email"]
+
+       # envia un correo electronicodel restablecimeinto de la contrasena
+       auth.send_password_reset_email(email)
+
+       return "se ha enviado un correo electronico para restablecer su Contrasena"
+    return render_template("reset_passwordcliente.html")
   
 #AUTH PROVEEDOR
 @app.route("/register_proveedor", methods=["GET","POST"])
